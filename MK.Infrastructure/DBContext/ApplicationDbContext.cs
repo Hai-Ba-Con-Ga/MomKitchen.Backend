f2f055505864b18d5d270b2cab16778354bc82fc
khoabui <buidangkhoa05@gmail.com>
2023-09-18 02:20:43 +0700

    (...) Update comment for ApplicationDbContextt

--- MK.Infrastructure/DBContext/ApplicationDbContext.cs
+++ MK.Infrastructure/DBContext/ApplicationDbContext.cs
@@ -34,6 +34,29 @@ namespace MK.Infrastructure.DBContext
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        
+    }
+/* Demo for EntityConfiguration
+    public class AccountEntityConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable("account");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName("id");
+            builder.Property(x => x.Username).HasColumnName("username");
+            builder.Property(x => x.Password).HasColumnName("password");
+            builder.Property(x => x.Email).HasColumnName("email");
+            builder.Property(x => x.Phone).HasColumnName("phone");
+            builder.Property(x => x.Role).HasColumnName("role");
+            builder.Property(x => x.Status).HasColumnName("status");
+            builder.Property(x => x.CreatedAt).HasColumnName("created_at");
+            builder.Property(x => x.UpdatedAt).HasColumnName("updated_at");
         }
     }
+*/
+ 
 }
