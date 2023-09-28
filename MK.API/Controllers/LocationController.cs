e11ab35f8bffe9ead93a5455fcb2c216379ae02b
khoabui <buidangkhoa05@gmail.com>
2023-09-28 15:43:23 +0700

    (...) Update Generic Repository

---
+++ MK.API/Controllers/LocationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MK.Domain.Dto.Response;
+
+namespace MK.API.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+
+        public LocationController()
+        {
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, typeof(IEnumerable<LocationRes>))]
+        public Task<IActionResult> GetAll()
+        {
+
+        }
+    }
+}
---
+++ MK.Domain/Dto/Response/LocationRes.cs
@@ -0,0 +1,23 @@
+﻿using MK.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MK.Domain.Dto.Response
+{
+    public class LocationRes
+    {
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+
+        public Kitchen? Kitchen { get; set; }
+
+        public Area? AreaAsNorth { get; set; }
+        public Area? AreaAsSouth { get; set; }
+        public Area? AreaAsWest { get; set; }
+        public Area? AreaAsEast { get; set; }
+    }
+}
